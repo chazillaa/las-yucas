3897bdf6aaf883028d1eb860d8fea6047288889c
Jacob Nollen <jacob.nollen@netplanner.com>
2023-04-04 23:03:01 -0400

    started to add seeds for testing. Currently only ingredients is working but the code for all seeds is now setup. Probably going to have to manually create menun items ingredients list unless someone wants to write code to make those. Wrote the code to create the shoppingcart because that is part of what we have to do anyways. Needs testing

--- server/server.js
+++ server/server.js
@@ -14,9 +14,10 @@ app.use(cors())
 
 app.use('/api', userRoutes)
 
-const CONNECTION_URL = 'mongodb://127.0.0.1:27017/las-yucas_db'
 const PORT = process.env.PORT|| 3001
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-
+mongoose.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server Running on Port: http://localhost:${PORT}`)
+  })
+});
