12e6c76157f8a414352a2ac30f5b6e1762d69e90
Isaac Cha <ijcha93@gmail.com>
2023-04-14 22:32:43 -0400

    heroku build

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose');
+const { ServerApiVersion } = require('mongodb');
 
 // Wrap Mongoose around local connection to MongoDB
 const connection_url = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/las-yucas_db'
-mongoose.connect(connection_url, {
+if (connection_url.includes('+srv')){
+  mongoose.connect(connection_url, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+  }})
+} else {
+  mongoose.connect(connection_url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+}
+
 
 // Export connection 
 module.exports = mongoose.connection;
