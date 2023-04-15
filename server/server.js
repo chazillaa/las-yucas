3c66df3f8fd48e000c3139097e34477e126a514e
Isaac Cha <ijcha93@gmail.com>
2023-04-15 00:13:33 -0400

    add path

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 
 const routes = require('./routes')
 const connection = require('./config/connection.js');
