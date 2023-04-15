73ae315947051be7ea9b02b290ef971a303d108d
Isaac Cha <ijcha93@gmail.com>
2023-04-15 16:09:11 -0400

    add footer

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
+    <div className="">
       <div className="p-5 text-center">
         <h1 className="mb-3">Home</h1>
       </div>
