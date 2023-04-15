04363a9a47435d374c04e23e41e5d4e80e587078
Isaac Cha <ijcha93@gmail.com>
2023-04-14 22:36:30 -0400

    server.js edit

--- server/server.js
+++ server/server.js
@@ -12,6 +12,14 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.use(routes)
 
 const PORT = process.env.PORT|| 3001
