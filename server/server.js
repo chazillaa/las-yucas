8dd0f9116d21c8efebd61df46aea13eb0387c6f3
Jacob Nollen <jacob.nollen@netplanner.com>
2023-04-16 21:29:58 -0400

    signup was not redirecting to home on success

--- server/server.js
+++ server/server.js
@@ -17,11 +17,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 } 
 
+app.use(routes);
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(routes)
 
 const PORT = process.env.PORT|| 3001
 
